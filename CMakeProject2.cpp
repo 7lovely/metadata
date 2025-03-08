ba58bf497f1c9cfebee6bc0f116fc0721fdeaa4e
unknown slayer <unknown slayer@METADATA>
2025-03-08 15:01:40 +0100

    Ghoul

--- CMakeProject2.cpp
+++ CMakeProject2.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-
 int main() {
-	for (int i = 1; i <= 10; i = i + 3) {
-		cout << i << " ";
-	}
-	
-
-	return 0;
+    for (int i = 1000; i > 7; i = i - 7) {
+        cout << i << " - " << "7" << " = " << i - 7 << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));  
+    }
+    cout << "Я гуль" << endl;
+    return 0;
 }
