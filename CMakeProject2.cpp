a7a4a2672caeab6a470df0643d022eccd40195f5
unknown slayer <unknown slayer@METADATA>
2025-03-08 14:58:14 +0100

    Add project files.

---
+++ CMakeProject2.cpp
@@ -0,0 +1,13 @@
+﻿#include "CMakeProject2.h"
+
+using namespace std;
+
+
+int main() {
+	for (int i = 1; i <= 10; i = i + 3) {
+		cout << i << " ";
+	}
+	
+
+	return 0;
+}
---
+++ CMakeProject2.h
@@ -0,0 +1,13 @@
+﻿// CMakeProject2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <conio.h>
+#include <cmath>
+#include <thread>
+
+// TODO: Reference additional headers your program requires here.
