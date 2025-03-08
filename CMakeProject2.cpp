e85f957e1b9b6396332023f7a387f7e0e634dfdc
unknown slayer <unknown slayer@METADATA>
2025-03-08 15:07:07 +0100

    fix2

--- CMakeProject2.cpp
+++ CMakeProject2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     for (int i = 1000; i > 7; i = i - 7) {
         cout << i << " - " << "7" << " = " << i - 7 << endl;
-        this_thread::sleep_for(chrono::milliseconds(1000));  
+        this_thread::sleep_for(chrono::milliseconds(20));  
     }
     cout << "Я гуль" << endl;
     return 0;
